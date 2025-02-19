73fee890ddf9ce82f9425bdf2eb872e32aefdcfd
Giulliafb <kauanbkzin@gmail.com>
2025-02-19 10:14:55 -0300

    testando novamente

--- src/mattbk/testando.java
+++ src/mattbk/testando.java
@@ -5,7 +5,37 @@ import java.util.Scanner;
 public class testando {
 
 	public static void main(String[] args) {
-	Scanner sc = new Scanner(System.in);
+	
+	double potencia = Math.pow(2, 3);
+	System.out.println(potencia);
+	
+	double raizQuadrada = Math.sqrt(16);
+	System.out.println(raizQuadrada);
+
+	double numeroAleatorio = Math.random();
+	System.out.printf("%.1f" , numeroAleatorio);
+	
+		
+		
+		
+		
+		
+		
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	/*Scanner sc = new Scanner(System.in);
 	
 	
 	String nome;
@@ -22,5 +52,8 @@ public class testando {
 	System.out.println("TU É GAY");
 	
 	sc.close();
+	*/
+	
+		
 	}
 }
